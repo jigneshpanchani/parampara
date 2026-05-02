e3fcbb9ae6962d39d8ae4f8e8a1a2b707bc1dc62
jignesh <jigneshpanchani1@gmail.com>
2026-05-02 12:03:33 +0530

    Update Invoice module

---
+++ app/Models/SellPayment.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SellPayment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'sell_id',
+        'payment_date',
+        'amount',
+        'payment_method',
+        'reference_number',
+        'notes',
+    ];
+
+    protected $casts = [
+        'payment_date' => 'date',
+        'amount' => 'decimal:2',
+    ];
+
+    public function sell()
+    {
+        return $this->belongsTo(Sell::class);
+    }
+
+    public function getPaymentMethodLabel(): string
+    {
+        return match ($this->payment_method) {
+            'cash' => 'Cash',
+            'upi' => 'UPI',
+            'gpay' => 'G-Pay',
+            'bank_transfer' => 'Bank Transfer',
+            'cheque' => 'Cheque',
+            'other' => 'Other',
+            default => ucfirst($this->payment_method ?? ''),
+        };
+    }
+}
